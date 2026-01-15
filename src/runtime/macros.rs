9d701283d375bbfd57da270d54802c68332be3c9
enzoblain <blenzodu57@gmail.com>
2026-01-15 13:47:52 +0100

    feat: macro v2

--- src/runtime/macros.rs
+++ src/runtime/macros.rs
@@ -1,14 +1,127 @@
 #[macro_export]
 macro_rules! join {
-    ( $( let $name:ident = $fut:expr ),* $(,)? ) => {{
-        $(
-            let $name = $crate::task::spawn(async move { $fut.await });
-        )*
+    ( $future:expr $(,)? ) => {{ $future.await }};
 
-        (
-            $(
-                $name.await
-            ),*
-        )
+    ( $f1:expr, $f2:expr $(,)? ) => {{
+        use std::future;
+        use std::future::Future;
+        use std::pin::Pin;
+        use std::task::{Context, Poll};
+
+        let mut __f1 = (Box::pin($f1), None, false);
+        let mut __f2 = (Box::pin($f2), None, false);
+
+        future::poll_fn(move |cx: &mut Context<'_>| {
+            if !__f1.2 {
+                if let Poll::Ready(val) = __f1.0.as_mut().poll(cx) {
+                    __f1.1 = Some(val);
+                    __f1.2 = true;
+                }
+            }
+            if !__f2.2 {
+                if let Poll::Ready(val) = __f2.0.as_mut().poll(cx) {
+                    __f2.1 = Some(val);
+                    __f2.2 = true;
+                }
+            }
+            if __f1.2 && __f2.2 {
+                Poll::Ready((__f1.1.take().unwrap(), __f2.1.take().unwrap()))
+            } else {
+                Poll::Pending
+            }
+        })
+        .await
+    }};
+
+    ( $f1:expr, $f2:expr, $f3:expr $(,)? ) => {{
+        use std::future;
+        use std::future::Future;
+        use std::pin::Pin;
+        use std::task::{Context, Poll};
+
+        let mut __f1 = (Box::pin($f1), None, false);
+        let mut __f2 = (Box::pin($f2), None, false);
+        let mut __f3 = (Box::pin($f3), None, false);
+
+        future::poll_fn(move |cx: &mut Context<'_>| {
+            if !__f1.2 {
+                if let Poll::Ready(val) = __f1.0.as_mut().poll(cx) {
+                    __f1.1 = Some(val);
+                    __f1.2 = true;
+                }
+            }
+            if !__f2.2 {
+                if let Poll::Ready(val) = __f2.0.as_mut().poll(cx) {
+                    __f2.1 = Some(val);
+                    __f2.2 = true;
+                }
+            }
+            if !__f3.2 {
+                if let Poll::Ready(val) = __f3.0.as_mut().poll(cx) {
+                    __f3.1 = Some(val);
+                    __f3.2 = true;
+                }
+            }
+            if __f1.2 && __f2.2 && __f3.2 {
+                Poll::Ready((
+                    __f1.1.take().unwrap(),
+                    __f2.1.take().unwrap(),
+                    __f3.1.take().unwrap(),
+                ))
+            } else {
+                Poll::Pending
+            }
+        })
+        .await
+    }};
+
+    ( $f1:expr, $f2:expr, $f3:expr, $f4:expr $(,)? ) => {{
+        use std::future;
+        use std::future::Future;
+        use std::pin::Pin;
+        use std::task::{Context, Poll};
+
+        let mut __f1 = (Box::pin($f1), None, false);
+        let mut __f2 = (Box::pin($f2), None, false);
+        let mut __f3 = (Box::pin($f3), None, false);
+        let mut __f4 = (Box::pin($f4), None, false);
+
+        future::poll_fn(move |cx: &mut Context<'_>| {
+            if !__f1.2 {
+                if let Poll::Ready(val) = __f1.0.as_mut().poll(cx) {
+                    __f1.1 = Some(val);
+                    __f1.2 = true;
+                }
+            }
+            if !__f2.2 {
+                if let Poll::Ready(val) = __f2.0.as_mut().poll(cx) {
+                    __f2.1 = Some(val);
+                    __f2.2 = true;
+                }
+            }
+            if !__f3.2 {
+                if let Poll::Ready(val) = __f3.0.as_mut().poll(cx) {
+                    __f3.1 = Some(val);
+                    __f3.2 = true;
+                }
+            }
+            if !__f4.2 {
+                if let Poll::Ready(val) = __f4.0.as_mut().poll(cx) {
+                    __f4.1 = Some(val);
+                    __f4.2 = true;
+                }
+            }
+            if __f1.2 && __f2.2 && __f3.2 && __f4.2 {
+                Poll::Ready((
+                    __f1.1.take().unwrap(),
+                    __f2.1.take().unwrap(),
+                    __f3.1.take().unwrap(),
+                    __f4.1.take().unwrap(),
+                ))
+            } else {
+                Poll::Pending
+            }
+        })
+        .await
     }};
 }
--- tests/join.rs
+++ tests/join.rs
@@ -7,7 +7,7 @@ fn test_join_single_future() {
     let rt = RuntimeBuilder::new().build();
 
     let result = rt.block_on(async {
-        let a = join!(let a = async { 42 });
+        let a = join!(async { 42 });
         a
     });
 
@@ -19,10 +19,7 @@ fn test_join_two_futures() {
     let rt = RuntimeBuilder::new().build();
 
     let result = rt.block_on(async {
-        let (a, b) = join!(
-            let a = async { 10 },
-            let b = async { 20 }
-        );
+        let (a, b) = join!(async { 10 }, async { 20 });
         (a, b)
     });
 
@@ -34,11 +31,7 @@ fn test_join_three_futures() {
     let rt = RuntimeBuilder::new().build();
 
     let result = rt.block_on(async {
-        let (a, b, c) = join!(
-            let a = async { "hello" },
-            let b = async { 42 },
-            let c = async { true }
-        );
+        let (a, b, c) = join!(async { "hello" }, async { 42 }, async { true });
         (a, b, c)
     });
 
@@ -56,13 +49,13 @@ fn test_join_concurrent_execution() {
 
     rt.block_on(async move {
         join!(
-            let _a = async move {
+            async move {
                 c1.fetch_add(1, Ordering::SeqCst);
             },
-            let _b = async move {
+            async move {
                 c2.fetch_add(10, Ordering::SeqCst);
             },
-            let _c = async move {
+            async move {
                 c3.fetch_add(100, Ordering::SeqCst);
             }
         );
@@ -76,10 +69,7 @@ fn test_join_with_trailing_comma() {
     let rt = RuntimeBuilder::new().build();
 
     let result = rt.block_on(async {
-        let (a, b) = join!(
-            let a = async { 1 },
-            let b = async { 2 },
-        );
+        let (a, b) = join!(async { 1 }, async { 2 },);
         a + b
     });
 
@@ -91,12 +81,10 @@ fn test_join_different_types() {
     let rt = RuntimeBuilder::new().build();
 
     let result = rt.block_on(async {
-        let (num, text, vec) = join!(
-            let num = async { 100i32 },
-            let text = async { String::from("test") },
-            let vec = async { vec![1, 2, 3] }
-        );
-        (num, text, vec)
+        let (num, text, v) = join!(async { 100i32 }, async { String::from("test") }, async {
+            vec![1, 2, 3]
+        });
+        (num, text, v)
     });
 
     assert_eq!(result.0, 100);
@@ -111,10 +99,9 @@ fn test_join_with_captured_values() {
     let multiplier = 2;
 
     let result = rt.block_on(async move {
-        let (a, b) = join!(
-            let a = async move { value * multiplier },
-            let b = async move { value + multiplier }
-        );
+        let (a, b) = join!(async move { value * multiplier }, async move {
+            value + multiplier
+        });
         (a, b)
     });
 
@@ -126,12 +113,10 @@ fn test_join_nested_async() {
     let rt = RuntimeBuilder::new().build();
 
     let result = rt.block_on(async {
-        let outer = join!(
-            let outer = async {
-                let inner = async { 42 };
-                inner.await * 2
-            }
-        );
+        let outer = join!(async {
+            let inner = async { 42 };
+            inner.await * 2
+        });
         outer
     });
 
@@ -148,10 +133,10 @@ fn test_join_order_independence() {
 
     rt.block_on(async move {
         join!(
-            let _a = async move {
+            async move {
                 o1.lock().unwrap().push(1);
             },
-            let _b = async move {
+            async move {
                 o2.lock().unwrap().push(2);
             }
         );
@@ -168,10 +153,7 @@ fn test_join_with_option_results() {
     let rt = RuntimeBuilder::new().build();
 
     let result = rt.block_on(async {
-        let (a, b) = join!(
-            let a = async { Some(42) },
-            let b = async { None::<i32> }
-        );
+        let (a, b) = join!(async { Some(42) }, async { None::<i32> });
         (a, b)
     });
 
@@ -184,10 +166,9 @@ fn test_join_with_result_types() {
     let rt = RuntimeBuilder::new().build();
 
     let result = rt.block_on(async {
-        let (ok_result, err_result) = join!(
-            let ok_result = async { Ok::<i32, &str>(100) },
-            let err_result = async { Err::<i32, &str>("error") }
-        );
+        let (ok_result, err_result) = join!(async { Ok::<i32, &str>(100) }, async {
+            Err::<i32, &str>("error")
+        });
         (ok_result, err_result)
     });
 
